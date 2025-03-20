2f0c25f7bb1016f24d3080f97f120e25c08294bb
Abdulkarim Maslouk <90416002+Abdulkarim9@users.noreply.github.com>
2025-03-21 00:40:21 +0300

    Delete start-server.bat

--- start-server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting AnimX development server...
-echo.
-echo Open your browser and navigate to http://localhost:8000
-echo Press Ctrl+C to stop the server
-echo.
-python -m http.server 
